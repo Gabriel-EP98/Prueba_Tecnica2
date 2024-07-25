e5ad0d4eadd013fa23017fcd207b0db9481b3b90
unknown <Gabriel-EP98>
2024-07-24 21:07:00 -0600

    Mi Practica Tenica 2

---
+++ ProductsData.cs
@@ -0,0 +1,8 @@
+﻿namespace Prueba_Tenica2
+{
+    public class ProductsData
+    {
+        public string Name { get; set; }
+        public double Total { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using Prueba_Tenica2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string currentDirectory = Directory.GetCurrentDirectory();
+        string filePath = $@"{currentDirectory}/products.txt";
+        List<ProductsData> productsCollection = GetProductsData(filePath);
+        if (productsCollection.Count > 0)
+            ShowInformationData(productsCollection);
+        else
+            Console.WriteLine("El archivo no contiene datos, favor de verificar");
+    }
+
+    public static List<ProductsData> GetProductsData(string filePath)
+    {
+        string[] fileData = File.ReadAllLines(filePath);
+
+        List<ProductsData> productsCollection = new List<ProductsData>();
+        foreach (string data in fileData)
+        {
+            if (string.IsNullOrEmpty(data)) continue;
+            string[] dataInformation = data.Split('|');
+            ProductsData productData = SetProductInfo(dataInformation);
+            productsCollection.Add(productData);
+        }
+
+        return productsCollection;
+    }
+
+    private static ProductsData SetProductInfo(string[] dataInformation)
+    {
+        const int productName = 0;
+        const int total = 3;
+
+        return new ProductsData()
+        {
+            Name = dataInformation[productName],
+            Total = Convert.ToDouble(string.IsNullOrEmpty(dataInformation[total]) ? "0" : dataInformation[total])
+        };
+    }
+
+    private static void ShowInformationData(List<ProductsData> productsInformation)
+    {
+        double totalPrice = 0;
+        productsInformation.ForEach(product => totalPrice += product.Total);
+
+        List<string> uniqueProducts = productsInformation.Select(p => p.Name).Distinct().ToList();
+
+        Console.WriteLine($"Total: {totalPrice}");
+        Console.WriteLine("Productos únicos:");
+        uniqueProducts.ForEach(product => Console.WriteLine($"* {product}"));
+    }
+}
